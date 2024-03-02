3a8606abfa35879a1905a66de1bc2361dee22012
8sileus <1926004708@qq.com>
2024-03-03 00:19:59 +0800

    Update interval and sleep

--- zedio/time/interval.hpp
+++ zedio/time/interval.hpp
@@ -2,38 +2,96 @@
 
 #include "zedio/common/macros.hpp"
 #include "zedio/time/sleep.hpp"
+// C++
+#include <utility>
 
 namespace zedio::time {
+
+enum class MissedTickBehavior {
+    Burst,
+    Delay,
+    Skip,
+};
 
 namespace detail {
 
     class Interval {
     public:
-        explicit Interval(Sleep start, std::chrono::nanoseconds period)
-            : sleep_{start}
-            , period_{period} {};
+        Interval(std::chrono::steady_clock::time_point first_expired_time,
+                 std::chrono::nanoseconds              period,
+                 MissedTickBehavior                    behavior = MissedTickBehavior::Burst)
+            : expired_time_{first_expired_time}
+            , period_{period}
+            , behavior_{behavior} {};
 
         [[REMEMBER_CO_AWAIT]]
         auto tick() noexcept -> Sleep {
-            auto sleep = sleep_;
-            sleep_.duration_ = period_;
-            return sleep;
+            auto expired_time = expired_time_;
+            expired_time_ = next_timeout();
+            return Sleep{expired_time};
+        }
+
+        [[nodiscard]]
+        auto period() const noexcept {
+            return period_;
+        }
+
+        void reset() noexcept {
+            expired_time_ = std::chrono::steady_clock::now() + period_;
+        }
+
+        void reset_immediately() noexcept {
+            expired_time_ = std::chrono::steady_clock::now();
+        }
+
+        void reset_after(std::chrono::nanoseconds after) noexcept {
+            expired_time_ = std::chrono::steady_clock::now() + after;
+        }
+
+        void reset_at(std::chrono::steady_clock::time_point deadline) noexcept {
+            expired_time_ = deadline;
+        }
+
+        [[nodiscard]]
+        auto missed_tick_behavior() const noexcept -> MissedTickBehavior {
+            return behavior_;
+        }
+
+        void set_missed_tick_behavior(MissedTickBehavior behavior) noexcept {
+            behavior_ = behavior;
         }
 
     private:
-        Sleep                    sleep_;
-        std::chrono::nanoseconds period_;
+        auto next_timeout() -> std::chrono::steady_clock::time_point {
+            auto now = std::chrono::steady_clock::now();
+            switch (behavior_) {
+            case MissedTickBehavior::Burst:
+                return expired_time_ + period_;
+            case MissedTickBehavior::Delay:
+                return now + period_;
+            case MissedTickBehavior::Skip:
+                return now + period_
+                       - std::chrono::nanoseconds{((now - expired_time_).count())
+                                                  % period_.count()};
+            }
+            std::unreachable();
+        }
+
+    private:
+        std::chrono::steady_clock::time_point expired_time_;
+        std::chrono::nanoseconds              period_;
+        MissedTickBehavior                    behavior_;
     };
 
 } // namespace detail
 
 static inline auto interval(std::chrono::nanoseconds duration) {
-    return detail::Interval{time::sleep(duration), duration};
+    return detail::Interval{std::chrono::steady_clock::now(), duration};
 }
 
 static inline auto interval_at(std::chrono::steady_clock::time_point start,
                                std::chrono::nanoseconds              duration) {
-    return detail::Interval{time::sleep_until(start), duration};
+    return detail::Interval{start, duration};
 }
 
 } // namespace zedio::time
--- zedio/time/sleep.hpp
+++ zedio/time/sleep.hpp
@@ -10,36 +10,44 @@ namespace zedio::time {
 namespace detail {
 
     class Sleep {
-        friend class Interval;
-
     public:
-        explicit Sleep(const std::chrono::nanoseconds &duration)
-            : duration_(duration) {}
+        explicit Sleep(std::chrono::steady_clock::time_point expired_time)
+            : expired_time_{expired_time} {}
 
         auto await_ready() const noexcept -> bool {
             return false;
         }
 
-        void await_suspend(std::coroutine_handle<> handle) noexcept {
-            t_timer->add_timer_event(handle, duration_);
+        auto await_suspend(std::coroutine_handle<> handle) noexcept {
+            if (expired_time_ > std::chrono::steady_clock::now() + 1ms) {
+                t_timer->add_timer_event(handle, expired_time_);
+                return true;
+            } else {
+                return false;
+            }
         }
 
         void await_resume() const noexcept {};
 
+        [[nodiscard]]
+        auto deadline() const noexcept -> std::chrono::steady_clock::time_point {
+            return expired_time_;
+        }
+
     private:
-        std::chrono::nanoseconds duration_{};
+        std::chrono::steady_clock::time_point expired_time_;
     };
 
 } // namespace detail
 
 [[REMEMBER_CO_AWAIT]]
-static inline auto sleep(std::chrono::nanoseconds duration) {
-    return detail::Sleep{duration};
+static inline auto sleep(const std::chrono::nanoseconds &duration) {
+    return detail::Sleep{std::chrono::steady_clock::now() + duration};
 }
 
 [[REMEMBER_CO_AWAIT]]
-static inline auto sleep_until(std::chrono::steady_clock::time_point start) {
-    return detail::Sleep{start - std::chrono::steady_clock::now()};
+static inline auto sleep_until(std::chrono::steady_clock::time_point expired_time) {
+    return detail::Sleep{expired_time};
 }
 
 } // namespace zedio::time
