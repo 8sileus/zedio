c1475571cc4d8881645f66a38b7dcf8f4b698d6b
8sileus <1926004708@qq.com>
2024-03-08 18:08:58 +0800

    Small update: add sync.hpp

---
+++ zedio/sync.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "zedio/sync/channel.hpp"
+#include "zedio/sync/condition_variable.hpp"
+#include "zedio/sync/latch.hpp"
+#include "zedio/sync/mutex.hpp"
+#include "zedio/sync/semaphore.hpp"
