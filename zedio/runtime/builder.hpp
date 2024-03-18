b634feed56e4ce43b4abd67422d4a7e8e38bac52
8sileus <1926004708@qq.com>
2024-03-18 23:34:13 +0800

    Fixed bug: set config failed

--- zedio/runtime/builder.hpp
+++ zedio/runtime/builder.hpp
@@ -15,79 +15,65 @@ class Builder {
 public:
     [[nodiscard]]
     auto build() -> R {
-        return R{std::move(config_)};
+        return R{this->config_};
     }
 
     [[nodiscard]]
     auto driver() -> DriverBuilder<R> {
-        return DriverBuilder<R>{config_};
+        return *static_cast<DriverBuilder<R> *>(this);
     }
 
     [[nodiscard]]
     auto scheduler() -> SchedulerBuilder<R> {
-        return SchedulerBuilder<R>{config_};
+        return *static_cast<SchedulerBuilder<R> *>(this);
     }
 
-private:
-    Config config_;
+protected:
+    Config config_{};
 };
 
 template <class R>
 class DriverBuilder : public Builder<R> {
 public:
-    DriverBuilder(Config &config)
-        : config_{config} {}
-
-public:
     [[nodiscard]]
     auto set_custom_flags(unsigned int flag) -> DriverBuilder & {
-        config_.ring_flags_ |= flag;
+        this->config_.ring_flags_ |= flag;
         return *this;
     }
 
     [[nodiscard]]
     auto set_ring_entries(std::size_t ring_entries) -> DriverBuilder & {
-        config_.ring_entries_ = ring_entries;
+        this->config_.ring_entries_ = ring_entries;
         return *this;
     }
 
     [[nodiscard]]
     auto set_submit_interval(uint32_t interval) -> DriverBuilder & {
-        config_.submit_interval_ = interval;
+        this->config_.submit_interval_ = interval;
         return *this;
     }
-
-private:
-    Config &config_;
 };
 
 template <class R>
 class SchedulerBuilder : public Builder<R> {
 public:
-    SchedulerBuilder(Config &config)
-        : config_{config} {}
-
-public:
     [[nodiscard]]
     auto set_num_workers(std::size_t num) -> SchedulerBuilder & {
-        config_.num_workers_ = num;
+        this->config_.num_workers_ = num;
         return *this;
     }
 
     [[nodiscard]]
     auto set_check_io_interval(uint32_t interval) -> SchedulerBuilder & {
-        config_.check_io_interval_ = interval;
+        this->config_.check_io_interval_ = interval;
         return *this;
     }
 
     [[nodiscard]]
     auto set_check_gloabal_interval(uint32_t interval) -> SchedulerBuilder & {
-        config_.check_global_interval_ = interval;
+        this->config_.check_global_interval_ = interval;
         return *this;
     }
-
-private:
-    Config &config_;
 };
 
 } // namespace zedio::runtime::detail
